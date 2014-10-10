8ef995a794dcf5f590f437b2bed5f96b1ad2afd0
Chandan Kumar <chandan.jhun@gmail.com>
2014-10-10 13:13:23 +0530

    User session service

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -1,5 +1,7 @@
 angular.module('myApp.controllers', [])
-.controller('HomeController', function($scope, ArticleService){
+.controller('HomeController', function($scope, session, SessionService, Share, ArticleService){
+	$scope.user = session.user;
+	
 	ArticleService.getLatestFeed()
 	.then(function(data){
 		$scope.articles = data;
--- app/assets/javascripts/app/services.js
+++ app/assets/javascripts/app/services.js
@@ -1,4 +1,4 @@
-angular.module('myApp.services', [])
+angular.module('myApp.services', ['ngResource'])
 .factory('ArticleService', function($http, $q){
 	var service = {
 		getLatestFeed: function(){
@@ -20,4 +20,27 @@ angular.module('myApp.services', [])
 		}
 	};
 	return service;
-});
+})
+.factory('Share', function($resource){
+
+})
+.factory('SessionService', function($http, $q){
+	var service = {
+		getCurrentUser: function(){
+			if(service.isAuthenticated()){
+				return $q.when(service.currentUser);
+			} else {
+				return $http.get('/api/current_user').then(function(resp){
+					return service.currentUser = resp.data;
+				})
+			}
+
+		},
+		currentUser: null,
+		isAuthenticated: function(){
+			return !!service.currentUser;
+		}
+
+	};
+	return service;
+})
